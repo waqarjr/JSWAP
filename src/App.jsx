6ff213f7c272a0187881631037b1d338176cd98a
waqarjr <waqarjr03@gmail.com>
2025-06-05 12:26:29 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='m-4 bg-amber-200 inline-block text-amber-800'>this is A HEding</h1>
+    </>
+  )
+}
+
+export default App
